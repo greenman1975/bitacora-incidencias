8864767c21845041281c982ba786bb29c7bfe6ad
greenman1975 <greenman1975@users.noreply.github.com>
2026-05-14 22:18:03 -0600

    Fix rate limiter proxy trust for Render

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = 3000;
 
 // Security
 app.disable('x-powered-by');
+app.set('trust proxy', 1);
 app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
